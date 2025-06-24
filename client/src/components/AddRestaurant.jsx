d208d014b0c195fb80af778fd137b8c58e0ef28b
PPenn4 <daniel.wang154@gmail.com>
2025-06-24 00:13:07 -0400

    Integrate backend data with frontend with Context API

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,18 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
 
 const AddRestaurant = () => {
+    const [name, setName] = useState("");
+    const [location, setLoc] = useState("");
+    const [price_range, setPrice] = useState("Price Range");
     return (
         <div className="mb-4">
             <form action="">
                 <div className="row gx-2 align-items-center">
                     <div className="col">
-                        <input type="text" className="form-control" placeholder="name" />
+                        <input type="text" value = {name} onChange={e => setName(e.target.value)} className="form-control" placeholder="name" />
                     </div>
                     <div className="col">
-                        <input type="text" className="form-control" placeholder="location" />  
+                        <input type="text" value = {location} onChange={e => setLoc(e.target.value)} className="form-control" placeholder="location" />  
                     </div>
                     <div className="col">
-                        <select className="form-select">
+                        <select className="form-select" value = {price_range} onChange={e => setPrice(e.target.value)} >
                             <option disabled>Price Range</option>
                             <option value="1">$</option>
                             <option value="2">$$</option>
--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -32,14 +32,25 @@ const RestaurantList = (props) => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
+                    {restaurants && restaurants.map(restaurant => {
+                        return(
+                        <tr key={restaurant.id}>
+                            <td>{restaurant.name}</td>
+                            <td>{restaurant.location}</td>
+                            <td>{"$".repeat(restaurant.price_range)}</td>
+                            <td>reviews</td>
+                            <td><button className="btn btn-warning">Update</button></td>
+                            <td><button className="btn btn-danger">Delete</button></td>
+                        </tr>
+                    )})}
+                    {/* <tr>
                         <td>test rest</td>
                         <td>test loc</td>
                         <td>$$</td>
                         <td>test</td>
                         <td><button className="btn btn-warning">Update</button></td>
                         <td><button className="btn btn-danger">Delete</button></td>
-                    </tr>
+                    </tr> */}
                 </tbody>
             </table>
         </div>
